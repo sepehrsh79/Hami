d1b60da734e3f08c1543723c34764c46526a5998
sepehrsh <sepehrshafiee1379@gmail.com>
2022-08-12 16:54:42 +0430

    fix

--- setting/forms.py
+++ setting/forms.py
@@ -4,47 +4,46 @@ from django import forms
 class SiteSettingForm(forms.Form):
     title = forms.CharField(
         widget=forms.TextInput(attrs={'placeholder': 'لطفا عنوان را وارد نمایید', 'class': 'form-control'}),
-        label='عنوان'
+        label='عنوان', required=False
     )
     address = forms.CharField(
         widget=forms.TextInput(attrs={'placeholder': 'لطفا آدرس را وارد نمایید', 'class': 'form-control'}),
-        label='آدرس'
+        label='آدرس', required=False
     )
 
     phone = forms.IntegerField(
         widget=forms.NumberInput(attrs={'placeholder': 'لطفا  شماره تماس را وارد نمایید', 'class': 'form-control'}),
-        label='شماره تماس',
+        label='شماره تماس', required=False
     )
 
     mobile = forms.IntegerField(
         widget=forms.NumberInput(attrs={'placeholder': 'لطفا شماره موبایل را وارد نمایید', 'class': 'form-control'}),
-        label='شماره موبایل',
+        label='شماره موبایل', required=False
     )
 
     fax = forms.IntegerField(
         widget=forms.NumberInput(attrs={'placeholder': 'لطفا فکس را وارد نمایید', 'class': 'form-control'}),
-        label='فکس',
+        label='فکس', required=False
     )
 
     email = forms.CharField(
         widget=forms.TextInput(attrs={'placeholder': 'لطفا ایمیل را وارد نمایید', 'class': 'form-control'}),
-        label='ایمیل',
+        label='ایمیل', required=False
     )
     
     about_us = forms.CharField(
         widget=forms.TextInput(attrs={'placeholder': 'لطفا درباره ما را وارد نمایید', 'class': 'form-control'}),
-        label='درباره ما',
+        label='درباره ما', required=False
     )
 
     copy_right = forms.CharField(
         widget=forms.TextInput(attrs={'placeholder': 'لطفا متن کپی رایت را وارد نمایید', 'class': 'form-control'}),
-        label='متن کپی رایت',
+        label='متن کپی رایت', required=False
     )
 
     logo_image = forms.ImageField(
         widget=forms.FileInput(attrs={'placeholder': 'لطفا لوگو سایت را وارد نمایید', 'class': 'form-control'}),
-        label='لوگو سایت',
-        required=False
+        label='لوگو سایت', required=False
     )
 
 
--- setting/models.py
+++ setting/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class SiteSetting(models.Model):
-    title = models.CharField(max_length=150, verbose_name='عنوان سایت')
-    address = models.CharField(max_length=400, verbose_name='آدرس')
-    phone = models.CharField(max_length=50, verbose_name='تلفن')
-    mobile = models.CharField(max_length=50, verbose_name='تلفن همراه')
-    fax = models.CharField(max_length=50, verbose_name='فکس')
-    email = models.EmailField(max_length=50, verbose_name='ایمیل')
+    title = models.CharField(max_length=150, verbose_name='عنوان سایت', null=True, blank=True)
+    address = models.CharField(max_length=400, verbose_name='آدرس', null=True, blank=True)
+    phone = models.CharField(max_length=50, verbose_name='تلفن', null=True, blank=True)
+    mobile = models.CharField(max_length=50, verbose_name='تلفن همراه', null=True, blank=True)
+    fax = models.CharField(max_length=50, verbose_name='فکس', null=True, blank=True)
+    email = models.EmailField(max_length=50, verbose_name='ایمیل', null=True, blank=True)
     about_us = models.TextField(verbose_name='درباره ', null=True, blank=True)
     copy_right = models.CharField(verbose_name='متن کپی رایت', null=True, blank=True, max_length=200)
     logo_image = models.ImageField(upload_to='logo/', null=True, blank=True, verbose_name='تصویر لوگو')
